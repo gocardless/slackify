abe990955cdc745c0431183ae9ad2aa3fc878677
Chris Sinjakli <chris@gocardless.com>
2017-07-28 17:06:41 +0100

    Add user model

---
+++ lib/slackify_web/models/user.ex
@@ -0,0 +1,10 @@
+defmodule Slackify.User do
+  use Ecto.Schema
+
+  @primary_key false
+  schema "users" do
+    field :slack_id, :string, primary_key: true
+    field :slack_access_token, :string
+    field :spotify_refresh_token, :string
+  end
+end
